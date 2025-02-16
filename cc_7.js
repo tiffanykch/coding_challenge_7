55d9880fae6e93b863ae0329f13a3c6fa6ad59ad
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-02-15 23:04:56 -0500

    Removed `return` from Task 1

--- cc_7.js
+++ cc_7.js
@@ -6,10 +6,24 @@ function calculateInvoice(subtotal, taxRate, discount) {
     
     // Console log formula output
     console.log(`Total Invoice: $${total.toFixed(2)}`);
-    return total;
 }
 
 // Test Data
 calculateInvoice(100, 0.08, 5);
 
 calculateInvoice(500, 0.1, 20);
+
+// TASK 2: FUNCTION EXPRESSION - EMPLOYEE HOURLY WAGE CALCULATION
+
+// Create a function expression to calculate hourly wage
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52);
+    
+    // Console log formula output
+    console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`)
+}
+
+// Test Data
+calculateHourlyWage(52000, 40);
+
+calculateHourlyWage(75000, 35);
